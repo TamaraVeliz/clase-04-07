6f69ce304be4d90e74ac171e1d8c44840994a803
TamaraVeliz <tamara.f.veliz@gmail.com>
2019-07-05 22:44:17 -0300

    ejercicios listos menos las sumas

---
+++ ejercicio.js
@@ -0,0 +1,14 @@
+var nombre;
+var apellido;
+var edad;
+var fechaDeNacimiento;
+var direccion;
+
+nombre = 'Sheldon';
+apellido = 'Zenith';
+edad = 22;
+fechaDeNacimiento = '21/07/1996';
+direccion = 'Martin Rodriguez 733';
+
+var nombre = prompt('Ingrese su nombre');
+console.log(nombre);
